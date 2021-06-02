298e4d244b3af96aec762c31aad73ef37f2c3508
zhuang-jia-xu <chuangjiaxu@gmail.com>
2021-06-02 10:33:37 +0800

    update GUI

--- Domino-Bot-Path-Setter/main.py
+++ Domino-Bot-Path-Setter/main.py
@@ -1,16 +1,34 @@
 # This Python file uses the following encoding: utf-8
 import os
-from pathlib import Path
 import sys
 
 from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import QQmlApplicationEngine
+from PySide2.QtCore import QObject, Slot, Signal, QTimer
+
+
+class MainWindow(QObject):
+    def __init__(self):
+        QObject.__init__(self)
+
+    setName = Signal(str)
+
+    @Slot(str)
+    def welcomeText(self, name):
+        self.setName.emit("Welcome, " + name)
 
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    engine.load(os.fspath(Path(__file__).resolve().parent / "qml/main.qml"))
+
+    # Get Context
+    main = MainWindow()
+    engine.rootContext().setContextProperty("backend", main)
+
+    # Load QML File
+    engine.load(os.path.join(os.path.dirname(__file__), "qml/main.qml"))
+
     if not engine.rootObjects():
         sys.exit(-1)
     sys.exit(app.exec_())
